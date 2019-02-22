e7e59033d0719af322dfe9c5894594132cc93832
sfmajors373 <sfmajors373@gmail.com>
2019-02-22 06:07:28 -0500

    Added things for flyway, will build, errors out when attempting to run

---
+++ src/main/resources/db/migration/V20190222051433__InitialTables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS todo (
+    todoid      INTEGER  PRIMARY KEY AUTOINCREMENT
+                         NOT NULL,
+    description STRING   NOT NULL,
+    userid      INTEGER  REFERENCES users (userid)
+                         NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    userid   INTEGER PRIMARY KEY AUTOINCREMENT
+                     NOT NULL,
+    username STRING  NOT NULL
+);
